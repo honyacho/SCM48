2d73e336b5dd231158144c92fb53f332690be2b1
honyacho <cc_called_pizza@yahoo.co.jp>
2020-02-23 04:23:40 +0900

    recoganizable Record name

--- app/Main.hs
+++ app/Main.hs
@@ -13,36 +13,35 @@ symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 spaces :: Parser ()
 spaces = skipMany1 space
 
-data LispVal = Atom String
-             | List [LispVal]
-             | DottedList [LispVal] LispVal
-             | Number Integer
-             | String String
-             | Bool Bool
-  -- deriving Show
+data LispVal = LSAtom String
+             | LSList [LispVal]
+             | LSDottedList [LispVal] LispVal
+             | LSNumber Integer
+             | LSString String
+             | LSBool Bool
 
-data LispError = NumArgs Integer [LispVal]
-               | TypeMismatch String LispVal
-               | Parser ParseError
-               | BadSpecialForm String LispVal
-               | NotFunction String String
-               | UnboundVar String String
-               | Default String
+data LispError = LENumArgs Integer [LispVal]
+               | LETypeMismatch String LispVal
+               | LEParser ParseError
+               | LEBadSpecialForm String LispVal
+               | LENotFunction String String
+               | LEUnboundVar String String
+               | LEDefault String
 
 showError :: LispError -> String
-showError (UnboundVar message varname) = message ++ ": " ++ varname
-showError (BadSpecialForm message form) = message ++ ": " ++ show form
-showError (NotFunction message func) = message ++ ": " ++ show func
-showError (NumArgs expected found) = "Expected " ++ show expected
+showError (LEUnboundVar message varname) = message ++ ": " ++ varname
+showError (LEBadSpecialForm message form) = message ++ ": " ++ show form
+showError (LENotFunction message func) = message ++ ": " ++ show func
+showError (LENumArgs expected found) = "Expected " ++ show expected
                                   ++ " args; found values " ++ unwordsList found
-showError (TypeMismatch expected found) = "Invalid type: expected " ++ expected
+showError (LETypeMismatch expected found) = "Invalid type: expected " ++ expected
                                        ++ ", found " ++ show found
-showError (Parser parseErr) = "Parse error at " ++ show parseErr
+showError (LEParser parseErr) = "Parse error at " ++ show parseErr
 
 instance Show LispError where show = showError
 instance Error LispError where
-  noMsg = Default "An error has occurred"
-  strMsg = Default
+  noMsg = LEDefault "An error has occurred"
+  strMsg = LEDefault
 type ThrowsError = Either LispError
 
 trapError action = catchError action (return . show)
@@ -52,7 +51,7 @@ extractValue (Right val) = val
 
 readExpr :: String -> ThrowsError LispVal
 readExpr input = case parse parseExpr "lisp" input of
-  Left err -> throwError $ Parser err
+  Left err -> throwError $ LEParser err
   Right val -> return val
 
 
@@ -62,13 +61,13 @@ unwordsList = unwords . map showVal
 instance Show LispVal where show = showVal
 
 showVal :: LispVal -> String
-showVal (String contents) = "\"" ++ contents ++ "\""
-showVal (Atom name) = name
-showVal (Number contents) = show contents
-showVal (Bool True) = "#t"
-showVal (Bool False) = "#f"
-showVal (List contents) = "(" ++ unwordsList contents ++ ")"
-showVal (DottedList head tail) = "(" ++ unwordsList head ++ " . " ++ showVal tail ++ ")"
+showVal (LSString contents) = "\"" ++ contents ++ "\""
+showVal (LSAtom name) = name
+showVal (LSNumber contents) = show contents
+showVal (LSBool True) = "#t"
+showVal (LSBool False) = "#f"
+showVal (LSList contents) = "(" ++ unwordsList contents ++ ")"
+showVal (LSDottedList head tail) = "(" ++ unwordsList head ++ " . " ++ showVal tail ++ ")"
 
 escapeStr :: Parser String
 escapeStr = do
@@ -81,7 +80,7 @@ parseString = do
   char '"'
   x <- many $ escapeStr <|> many1 (noneOf "\\\"")
   char '"'
-  return $ String $ foldl (++) "" x
+  return $ LSString $ foldl (++) "" x
 
 parseAtom :: Parser LispVal
 parseAtom = do
@@ -89,27 +88,27 @@ parseAtom = do
   rest <- many (letter <|> digit <|> symbol)
   let atom = first:rest
   return $ case atom of
-    "#t" -> Bool True
-    "#f" -> Bool False
-    _    -> Atom atom
+    "#t" -> LSBool True
+    "#f" -> LSBool False
+    _    -> LSAtom atom
 
 parseDecimal :: Parser LispVal
 parseDecimal = do
   optional $ try $ string "#d"
   digs <- many1 digit
-  return $ Number $ read digs
+  return $ LSNumber $ read digs
 
 parseOct :: Parser LispVal
 parseOct = do
   try $ string "#o"
   digs <- many1 $ oneOf "01234567"
-  return $ Number $ fst $ readOct digs !! 0
+  return $ LSNumber $ fst $ readOct digs !! 0
 
 parseHex :: Parser LispVal
 parseHex = do
   try $ string "#x"
   digs <- many1 $ oneOf "0123456789abcdef"
-  return $ Number $ fst $ readHex digs !! 0
+  return $ LSNumber $ fst $ readHex digs !! 0
 
 parseNumber :: Parser LispVal
 -- parseNumber = liftM (Number . read) $ many1 digit
@@ -128,19 +127,19 @@ parseExpr = parseNumber
     return x
 
 parseList :: Parser LispVal
-parseList = liftM List $ sepBy parseExpr spaces
+parseList = liftM LSList $ sepBy parseExpr spaces
 
 parseDottedList :: Parser LispVal
 parseDottedList = do
   head <- endBy parseExpr spaces
   tail <- char '.' >> spaces >> parseExpr
-  return $ DottedList head tail
+  return $ LSDottedList head tail
 
 parseQuoted :: Parser LispVal
 parseQuoted = do
   char '\''
   x <- parseExpr
-  return $ List [Atom "quote", x]
+  return $ LSList [LSAtom "quote", x]
 
 -- apply :: String -> [LispVal] -> LispVal
 -- apply func args = maybe (Bool False) ($ args) $ lookup func primitives
@@ -169,65 +168,65 @@ primitives = [("+", numericBinop (+)),
 
 
 eval :: LispVal -> ThrowsError LispVal
-eval val@(String _) = return val
-eval val@(Number _) = return val
-eval val@(Bool _) = return val
-eval (List [Atom "quote", val]) = return val
-eval (List (Atom func : args)) = mapM eval args >>= apply func
-eval badForm = throwError $ BadSpecialForm "Unrecognized special form" badForm
+eval val@(LSString _) = return val
+eval val@(LSNumber _) = return val
+eval val@(LSBool _) = return val
+eval (LSList [LSAtom "quote", val]) = return val
+eval (LSList (LSAtom func : args)) = mapM eval args >>= apply func
+eval badForm = throwError $ LEBadSpecialForm "Unrecognized special form" badForm
 
 apply :: String -> [LispVal] -> ThrowsError LispVal
 apply func args =
-  maybe (throwError $ NotFunction "Unrecognized primitive function args" func)
+  maybe (throwError $ LENotFunction "Unrecognized primitive function args" func)
         ($ args)
         (lookup func primitives)
 
 
 numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> ThrowsError LispVal
-numericBinop op singleVal@[_] = throwError $ NumArgs 2 singleVal
-numericBinop op params = mapM unpackNum params >>= return . Number . foldl1 op
+numericBinop op singleVal@[_] = throwError $ LENumArgs 2 singleVal
+numericBinop op params = mapM unpackNum params >>= return . LSNumber . foldl1 op
 
 unpackNum :: LispVal -> ThrowsError Integer
-unpackNum (Number n) = return n
-unpackNum (String n) = let parsed = reads n in
+unpackNum (LSNumber n) = return n
+unpackNum (LSString n) = let parsed = reads n in
                           if null parsed
-                            then throwError $ TypeMismatch "number" $ String n
+                            then throwError $ LETypeMismatch "number" $ LSString n
                             else return $ fst $ parsed !! 0
-unpackNum (List [n]) = unpackNum n
-unpackNum notNum = throwError $ TypeMismatch "number" notNum
+unpackNum (LSList [n]) = unpackNum n
+unpackNum notNum = throwError $ LETypeMismatch "number" notNum
 
 unpackStr :: LispVal -> ThrowsError String
-unpackStr (String s) = return s
-unpackStr (Number s) = return $ show s
-unpackStr (Bool s) = return $ show s
-unpackStr notString = throwError $ TypeMismatch "string" notString
+unpackStr (LSString s) = return s
+unpackStr (LSNumber s) = return $ show s
+unpackStr (LSBool s) = return $ show s
+unpackStr notString = throwError $ LETypeMismatch "string" notString
 
 unpackBool :: LispVal -> ThrowsError Bool
-unpackBool (Bool b) = return b
-unpackBool notBool = throwError $ TypeMismatch "boolean" notBool
+unpackBool (LSBool b) = return b
+unpackBool notBool = throwError $ LETypeMismatch "boolean" notBool
 
 boolBinop :: (LispVal -> ThrowsError a) -> (a -> a -> Bool) -> [LispVal] -> ThrowsError LispVal
 boolBinop unpacker op args = if length args /= 2
-                             then throwError $ NumArgs 2 args
+                             then throwError $ LENumArgs 2 args
                              else do left <- unpacker $ args !! 0
                                      right <- unpacker $ args !! 1
-                                     return $ Bool $ left `op` right
+                                     return $ LSBool $ left `op` right
 numBoolBinop = boolBinop unpackNum
 strBoolBinop = boolBinop unpackStr
 boolBoolBinop = boolBinop unpackBool
 
 car :: [LispVal] -> ThrowsError LispVal
-car [List (x : xs)] = return x
-car [DottedList (x : xs) _] = return x
-car [badArg] = throwError $ TypeMismatch "pair" badArg
-car badArgList = throwError $ NumArgs 1 badArgList
+car [LSList (x : xs)] = return x
+car [LSDottedList (x : xs) _] = return x
+car [badArg] = throwError $ LETypeMismatch "pair" badArg
+car badArgList = throwError $ LENumArgs 1 badArgList
 
 cdr :: [LispVal] -> ThrowsError LispVal
-cdr [List (x : xs)] = return $ List xs
-cdr [DottedList [xs] x] = return x
-cdr [DottedList (_ : xs) x] = return $ DottedList xs x
-cdr [badArg] = throwError $ TypeMismatch "pair" badArg
-cdr badArgList = throwError $ NumArgs 1 badArgList
+cdr [LSList (x : xs)] = return $ LSList xs
+cdr [LSDottedList [xs] x] = return x
+cdr [LSDottedList (_ : xs) x] = return $ LSDottedList xs x
+cdr [badArg] = throwError $ LETypeMismatch "pair" badArg
+cdr badArgList = throwError $ LENumArgs 1 badArgList
 
 main :: IO ()
 main = do
