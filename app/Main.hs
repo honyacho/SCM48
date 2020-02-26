0d73fc628d7ed5e7f392a8b8e752303ef480a646
honyacho <cc_called_pizza@yahoo.co.jp>
2020-02-27 01:42:49 +0900

    Add primitives

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ExistentialQuantification #-}
+
 module Main where
 
 import           System.Environment
@@ -7,6 +9,7 @@ import           Control.Monad
 import           Numeric
 import           Lib
 import           Control.Monad.Error
+import           System.IO
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
@@ -166,6 +169,12 @@ primitives =
   , ("mod"      , numericBinop mod)
   , ("quotient" , numericBinop quot)
   , ("remainder", numericBinop rem)
+  , ("car"      , car)
+  , ("cdr"      , cdr)
+  , ("cons"     , cons)
+  , ("eq?"      , eqv)
+  , ("eqv?"     , eqv)
+  , ("equal?"   , equal)
   ]
 
 
@@ -239,8 +248,75 @@ cdr [LSDottedList (_ : xs) x] = return $ LSDottedList xs x
 cdr [badArg                 ] = throwError $ LETypeMismatch "pair" badArg
 cdr badArgList                = throwError $ LENumArgs 1 badArgList
 
+cons :: [LispVal] -> ThrowsError LispVal
+cons [x1, LSList []            ] = return $ LSList [x1]
+cons [x , LSList xs            ] = return $ LSList $ x : xs
+cons [x , LSDottedList xs xlast] = return $ LSDottedList (x : xs) xlast
+cons [x1, x2                   ] = return $ LSDottedList [x1] x2
+cons badArgList                  = throwError $ LENumArgs 2 badArgList
+
+eqv :: [LispVal] -> ThrowsError LispVal
+eqv [(LSBool   arg1), (LSBool arg2)  ] = return $ LSBool $ arg1 == arg2
+eqv [(LSNumber arg1), (LSNumber arg2)] = return $ LSBool $ arg1 == arg2
+eqv [(LSString arg1), (LSString arg2)] = return $ LSBool $ arg1 == arg2
+eqv [(LSAtom   arg1), (LSAtom arg2)  ] = return $ LSBool $ arg1 == arg2
+eqv [(LSDottedList xs x), (LSDottedList ys y)] =
+  eqv [LSList $ xs ++ [x], LSList $ ys ++ [y]]
+eqv [(LSList arg1), (LSList arg2)] =
+  return
+    $  LSBool
+    $  (length arg1 == length arg2)
+    && (all eqvPair $ zip arg1 arg2)
+ where
+  eqvPair (x1, x2) = case eqv [x1, x2] of
+    Left  err          -> False
+    Right (LSBool val) -> val
+eqv [_, _]     = return $ LSBool False
+eqv badArgList = throwError $ LENumArgs 2 badArgList
+
+data Unpacker = forall a. Eq a => AnyUnpacker (LispVal -> ThrowsError a)
+unpackEquals :: LispVal -> LispVal -> Unpacker -> ThrowsError Bool
+unpackEquals arg1 arg2 (AnyUnpacker unpacker) =
+  do
+      unpacked1 <- unpacker arg1
+      unpacked2 <- unpacker arg2
+      return $ unpacked1 == unpacked2
+    `catchError` (const $ return False)
+
+equal :: [LispVal] -> ThrowsError LispVal
+equal [arg1, arg2] = do
+  primitiveEquals <- liftM or $ mapM
+    (unpackEquals arg1 arg2)
+    [AnyUnpacker unpackNum, AnyUnpacker unpackStr, AnyUnpacker unpackBool]
+  eqvEquals <- eqv [arg1, arg2]
+  return $ LSBool $ (primitiveEquals || let (LSBool x) = eqvEquals in x)
+equal badArgList = throwError $ LENumArgs 2 badArgList
+
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+evalString :: String -> IO String
+evalString expr =
+  return $ extractValue $ trapError (liftM show $ readExpr expr >>= eval)
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr = evalString expr >>= putStrLn
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred prompt action = do
+  result <- prompt
+  if pred result then return () else action result >> until_ pred prompt action
+
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "Lisp>>> ") evalAndPrint
+
 main :: IO ()
 main = do
-  args   <- getArgs
-  evaled <- return $ liftM show $ readExpr (args !! 0) >>= eval
-  putStrLn $ extractValue $ trapError evaled
+  args <- getArgs
+  case length args of
+    0         -> runRepl
+    1         -> evalAndPrint $ args !! 0
+    otherwise -> putStrLn "Program takes only 0 or 1 argument"
